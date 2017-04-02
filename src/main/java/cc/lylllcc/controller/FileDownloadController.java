190e15dd0c398e7cc2654b038916542ef4c0c159
yang liu <lylllcc@outlook.com>
2017-04-02 23:10:34 +0800

    改下载接口实现方式

--- src/main/java/cc/lylllcc/controller/FileDownloadController.java
+++ src/main/java/cc/lylllcc/controller/FileDownloadController.java
@@ -1,7 +1,5 @@
 package cc.lylllcc.controller;
 
-import com.sun.deploy.net.HttpResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
