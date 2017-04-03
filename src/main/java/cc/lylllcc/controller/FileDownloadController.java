813b021ceddb3d41bb60ae8c29c184084c634fe0
yang liu <lylllcc@outlook.com>
2017-04-03 20:31:39 +0800

    更换测试接口

--- src/main/java/cc/lylllcc/controller/FileDownloadController.java
+++ src/main/java/cc/lylllcc/controller/FileDownloadController.java
@@ -1,36 +1,83 @@
 package cc.lylllcc.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
- * Created by lylllcc on 2017/4/2.
+ * Created by lylllcc on 2017/4/3.
  */
 @RestController
 public class FileDownloadController {
 
-    @RequestMapping("/apk/{filename:.+}")
-    public void fileDownload(HttpServletResponse response ,@PathVariable("filename") String filename) throws FileNotFoundException {
-        InputStream inStream = new FileInputStream("res/apk/" + filename);
-        response.reset();
-        response.setContentType("bin");
-        response.addHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
-        byte[] b = new byte[100];
-        int len;
-        try {
-            while ((len = inStream.read(b)) > 0)
-                response.getOutputStream().write(b, 0, len);
-            inStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    /**
+     * Size of a byte buffer to read/write file
+     */
+    private static final int BUFFER_SIZE = 4096;
 
+    /**
+     * Path of the file to be downloaded, relative to application's directory
+     */
+    private String filePath = "res/apk/efficient.apk";
+
+    /**
+     * Method for handling file download request from client
+     */
+    @RequestMapping(value = "/apk/efficient.apk",method = RequestMethod.GET)
+    public void doDownload(HttpServletRequest request,
+                           HttpServletResponse response) throws IOException {
+
+        // get absolute path of the application
+        ServletContext context = request.getServletContext();
+
+        // construct the complete absolute path of the file
+        String fullPath =  filePath;
+
+        System.out.println(fullPath);
+
+        File downloadFile = new File(fullPath);
+        FileInputStream inputStream = new FileInputStream(downloadFile);
+
+        // get MIME type of the file
+        String mimeType = context.getMimeType(fullPath);
+        if (mimeType == null) {
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+
+        // set content attributes for the response
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+
+        // set headers for the response
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"",
+                downloadFile.getName());
+        response.setHeader(headerKey, headerValue);
+
+        // get output stream of the response
+        OutputStream outStream = response.getOutputStream();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead = -1;
+
+        // write bytes read from the input stream into the output stream
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+
+        inputStream.close();
+        outStream.close();
+
+    }
 }
